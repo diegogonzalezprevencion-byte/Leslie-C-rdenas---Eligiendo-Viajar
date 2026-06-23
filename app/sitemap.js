4506b0ef67c377b53e29f00f75ae87b111c47e7d
diegogonzalezprevencion-byte <diego.gonzalez.prevencion@gmail.com>
2026-06-22 22:53:47 -0400

    leslie 5 error

--- app/sitemap.js
+++
@@ -1,8 +0,0 @@
-const baseUrl = 'https://eligiendo-viajar.vercel.app';
-
-export default function sitemap() {
-  return ['', '/destinos', '/cotizar', '/experiencias', '/contacto', '/politicas'].map((path) => ({
-    url: `${baseUrl}${path}`,
-    lastModified: new Date()
-  }));
-}
